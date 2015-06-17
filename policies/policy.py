cfdd59fcd0fdd2648c02926a835ad9d1285de00c
Bas Bootsma <bootsman@gmail.com>
2015-06-17 13:03:06 +0200

    Updated interface.

--- policies/policy.py
+++ policies/policy.py
@@ -1,13 +1,6 @@
 class Policy(object):
-    def choose_arm(self):
-        pass
-    	
-    def update(self, arm, reward):
-        pass
-
-class ContextualPolicy(object):
     def choose_arm(self, context):
         pass
-
-    def update(self, arm, context, reward):
+    	
+    def update(self, arm, reward, context):
         pass

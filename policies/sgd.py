612a6d0ba1be8f67eafc2e39df3545a5f24c6655
Fenno Vermeij <fennovj@gmail.com>
2015-06-19 12:22:31 +0200

    Updated SGDClassifier to Regressor

--- policies/sgd.py
+++ policies/sgd.py
@@ -5,10 +5,10 @@ from policies.policy import Policy
 import numpy as np
 import numpy.random as random
 from math import sqrt, log
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier, SGDRegressor
 
 class SGD(Policy):
-    def __init__(self, numberOfArms, numberOfContextVariables = 0 ):
+    def __init__(self, numberOfArms, numberOfContextVariables = 0, warm_start = 50):
         """
         Construct a new Thompson Sampling policy.
         :param numberOfArms: Number of arms.
@@ -20,20 +20,28 @@ class SGD(Policy):
         """
         self.n = numberOfArms
         self.d = numberOfContextVariables + 1
-        self.model = SGDClassifier(loss = 'log')
+        self.model = SGDRegressor(loss = 'squared_loss')
+        self.randomsleft = 50
 
     def choose(self, context = []):
-
-        b = np.hstack((context, 0))
+        if self.randomsleft > 0 or np.random.rand() < 0.05:
+            self.randomsleft -= 1
+            return np.random.randint(self.n)
+        b = np.hstack((np.zeros(self.n), context))
         rewards = np.zeros(self.n)
         for i in range(self.n):
-            b[-1] = i
+            b[i] = 1
             rewards[i] = self.model.predict(b)
-        return np.argmax(rewards)
+            b[i] = 0
+            #rewards[i] = self.model.predict_proba(b.reshape(1,self.d))[0,1]
+        return np.argmax(rewards)#np.random.choice(np.where(rewards == rewards.max())[0])#np.argmax(rewards)
 
     def update(self, arm, reward, context = []):
-        context = np.hstack((context, arm))
-        self.model.partial_fit(context, reward)
+        #rewards = np.round(reward)
+        context = np.hstack((np.zeros(self.n), context))
+        context[arm] = 1
+        #print context, reward
+        self.model.partial_fit(np.array([context]), np.array([reward]))
 
     def numberOfArms(self):
         return self.n
@@ -42,4 +50,4 @@ class SGD(Policy):
         return self.d - 1
 
     def name(self):
-        return "SGD Classifier Thompson"
+        return "SGD Classifier"

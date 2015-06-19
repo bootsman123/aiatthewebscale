7dcd1ac6bf2d8de3e2cb3a0ccf4dc3d11731a23b
Bas Bootsma <bootsman@gmail.com>
2015-06-19 10:08:14 +0200

    Updated analyzer to work properly with context.

--- test.py
+++ test.py
@@ -8,32 +8,49 @@ from policies.gibbssampling import GibbsSampling
 from analyzer import Analyzer
 
 # With context.
+'''
 numberOfArms = 4
-numberOfContextVariables = 0
-#payoffs = np.array([[0.25, 0.25, 0.25, 0.25], [0.7, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.7]])
-payoffs = np.array([0.1, 0.1, 0.1, 0.7])
-policy = GibbsSampling(numberOfArms, numberOfContextVariables)
+numberOfContextVariables = 3
+payoffs = np.array([[0.25, 0.25, 0.25, 0.25], [0.1, 0.7, 0.1, 0.1], [0.1, 0.1, 0.7, 0.1]])
+policy = ThompsonSampling(numberOfArms, numberOfContextVariables)
+'''
 
 # Without context.
-'''
 numberOfArms = 4
+numberOfContextVariables = 0
 payoffs = np.array([0.1, 0.1, 0.7, 0.1])
-policy = EpsilonGreedy(numberOfArms)
-'''
+policy = GibbsSampling(numberOfArms)
 
 # Run multi-armed bandit.
 analyzer = Analyzer(policy, payoffs)
 arms, rewards, contexts = analyzer.analyze()
 
+# Plot arms.
+plt.hist(arms, bins=np.arange(numberOfArms + 1))
+plt.show()
+
 # Plot accuracy:
 # The probability of the policy to select the best arm.
 # (Only usable in case in which you know what the best arm is.)
-percentageCorrectArms = np.divide(np.cumsum(arms == np.argmax(payoffs)), list(range(1, len(arms) + 1)))
+if policy.numberOfContextVariables() > 0:
+    for nContext in range(numberOfContextVariables):
+        payoffsForContext = payoffs[nContext, :]
 
-plt.plot(percentageCorrectArms)
-plt.suptitle('Accuracy of policy')
+        contextIndices = np.argmax(contexts, axis = 1) == nContext
+        armsForContext = arms[contextIndices]
+        armsDivider = np.array(list(range(1, len(arms) + 1)))[contextIndices]
+
+        percentageCorrectArms = np.divide(np.cumsum(armsForContext == np.argmax(payoffsForContext)), armsDivider)
+
+        plt.plot(armsDivider, percentageCorrectArms, label='Context {0}'.format(nContext))
+else:
+    percentageCorrectArms = np.divide(np.cumsum(arms == np.argmax(payoffs)), list(range(1, len(arms) + 1)))
+    plt.plot(percentageCorrectArms)
+
+plt.suptitle('Accuracy of {0}'.format(policy.name()))
 plt.ylabel('Probability of selecting best arm')
 plt.xlabel('Number of interactions')
+plt.legend()
 plt.show()
 
 # Plot regret:

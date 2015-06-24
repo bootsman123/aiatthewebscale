05c45505344c9e9192b4cb6f4e1dd837a018b2a4
Fenno Vermeij <fennovermeij@student.ru.nl>
2015-06-24 15:32:52 +0200

    Fixed few bugs in pricethompson, added optional draw function here as well

--- policies/pricethompson.py
+++ policies/pricethompson.py
@@ -3,7 +3,7 @@ __author__ = 'fenno_000'
 from policies.policy import Policy
 
 import numpy as np
-import numpy.polynomial.polynomial as polynomial
+from scipy.stats import multivariate_normal as mv
 
 class PriceSampling(Policy):
     def __init__(self, degree = 2):
@@ -21,22 +21,37 @@ class PriceSampling(Policy):
         self.f = np.zeros(self.d)
         self.v = 1 #R * sqrt((24.0/epsilon) * self.d * log(1.0/delta))
 
-    def choose(self, context = []):
-        muc = np.random.multivariate_normal(self.mu, self.v**2.0 * self.Binv)
+        self.muc = None
 
-        roots = polynomial.polyroots(muc)
-        roots = np.hstack((0.1, roots, 50))
-        rewards = [polynomial.polyval(x, muc) for x in roots]
-        return roots[np.argmax(rewards)]
+    def choose(self, context = []):
+
+        if self.muc is None:
+            self.muc = mv.rvs(self.mu, self.v**2.0 * self.Binv)
+
+        rewardpoly = np.poly1d(self.muc)
+        deriv = np.polyder(self.muc)
+
+        roots = np.roots(deriv)
+        roots = [x for x in roots if x.real == x and 0 < x and x <= 50]
+        roots = np.hstack((1, roots, 50))
+        rewards = [rewardpoly(x) for x in roots]
+        self.muc = None
+        return [roots[np.argmax(rewards)].real]
 
     def update(self, arm, reward, context = []):
 
-        b = [arm**float(x) for x in range(self.d)]
+        b = np.array([arm[0]**float(x) for x in range(self.d -1, -1, -1)])
 
         self.B = self.B + np.outer(b, b)
         self.Binv = np.linalg.inv(self.B)
         self.f = self.f + (b * reward)
         self.mu = np.dot(self.Binv, self.f)
+
+    def draw(self):
+        try:
+            self.muc = mv.rvs(self.mu, self.v**2.0 * self.Binv)
+        except np.linalg.linalg.LinAlgError:
+            self.draw()
 
 
     def name(self):
